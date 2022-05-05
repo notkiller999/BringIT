bdd7a88af1a108cc3dc50ecae0724ab027d79596
Vlad <notkiller999@gmail.com>
2022-05-05 16:35:03 +0300

    initial commit in OOP style, make module slider

---
+++ src/js/modules/slider.js
@@ -0,0 +1,60 @@
+export default class Slider {
+    constructor(page, btns) {
+        this.page = document.querySelector(page);
+        this.slides = this.page.children;
+        this.btns = document.querySelectorAll(btns);
+        this.slideInex = 1;
+    }
+
+    showSlide(n) {
+        this.slides.forEach(slide => {
+            slide.style.display = 'none';
+        });
+
+        if (n > this.slides.length) {
+            this.slideInex = 1;
+        }
+
+        if (n < 1) {
+            this.slideInex = this.slides.length;
+        }
+
+        this.slides[this.slideInex - 1].style.display = 'block';
+    }
+
+    plusSlide(n) {
+        this.showSlide(this.slideInex += n);
+    }
+
+    showPopup(popup) {
+        if (this.slideInex === 3) {
+            setTimeout(() => {
+                popup.classList.add('animated', 'fadeInUp');
+                popup.style.display = 'block';
+            }, 3000)
+        } else {
+            popup.style.display = 'none';
+            popup.classList.remove('animated', 'fadeInUp');
+        }
+    }
+
+    render() {
+        
+        this.showSlide(this.slideInex);
+        this.btns.forEach(btn => {
+            btn.addEventListener('click', () => {
+                this.plusSlide(1);
+                try {
+                    this.popup = document.querySelector('.hanson');
+                    this.showPopup(this.popup);
+                } catch (e) {}
+            });
+            btn.parentNode.previousElementSibling.addEventListener('click', () => {
+                this.slideInex = 1;
+                this.showSlide(this.slideInex);
+            })
+        });
+    }
+
+    
+}
